25390e464d216fe10255b64a85534aa1c031848b
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-11-06 23:14:58 +0200

    lesson 2

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+//-----------------------------------------------------------------------------------------------------//
+var age;
+age = 50;
+var names;
+names = 'Max';
+var toggle;
+toggle = true;
+var empty;
+empty = null;
+var notInitialize;
+notInitialize = undefined;
+var callback = function (a) { return 100 + a; };
+console.log(callback(5));
