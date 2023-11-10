980ec0c3d1307e12412f0a3cd5cee405fb10c0d9
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-11-10 23:24:01 +0200

    remember write ts code

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,32 @@ function add(num1: number, num2: number) {
 button.addEventListener("click", function() {
     console.log(add(+input1.value, +input2.value))
 } )
+
+type Page = {
+    title: string,
+    likes: number,
+    accounts: string[],
+    status: 'open' | 'close',
+    details?: {
+      createAt: Date,
+      updateAt: Date,
+    }
+  }
+  const page1: Page = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+      createAt: new Date('2021-01-01'),
+      updateAt: new Date('2021-05-01'),
+    }
+  }
+  
+  const page2: Page = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+  }
+  console.log(page1, page2)
