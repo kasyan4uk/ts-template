fe3b51a2654a4a2be37b64e211f3bd3d76dcf763
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-12-04 17:37:45 +0200

    some change

--- src/app.ts
+++ src/app.ts
@@ -10,65 +10,81 @@ button.addEventListener("click", function() {
     console.log(add(+input1.value, +input2.value))
 } )
 
-abstract class House {
-    protected door = false;
-    private tenants: Person[] = [];
+// abstract class House {
+//     protected door = false;
+//     private tenants: Person[] = [];
     
 
-    constructor(protected key: Key) {}
+//     constructor(protected key: Key) {}
 
-    comeIn(person: Person) {
-        if (!this.door) {
-            throw new Error ('Door is close')
-        }       
+//     comeIn(person: Person) {
+//         if (!this.door) {
+//             throw new Error ('Door is close')
+//         }       
 
-        this.tenants.push(person);
-        console.log('Person inside')
+//         this.tenants.push(person);
+//         console.log('Person inside')
 
+//     }
+
+//     abstract openDoor(key: Key): boolean
+// }
+
+// class MyHouse extends House {
+//     openDoor(key: Key) {
+//         if (key.getSignature() !== this.key.getSignature()) {
+//             throw new Error ('Key to another door')
+//         }
+//         return this.door = true;
+//     }
+// };
+
+// class Key {
+//     private signature: number;
+
+//     constructor () {
+//         this.signature = Math.floor(Math.random()*10);
+//     }
+
+//     getSignature(): number {
+//         return this.signature
+//     }
+// }
+
+// class Person {
+//     constructor (private key: Key) {}
+
+//     getKey(): Key {
+//         return  this.key
+//     }
+// }
+// const key = new Key();
+// console.log(key)
+
+// const house = new MyHouse(key);
+// console.log(house)
+
+// const person = new Person(key);
+// console.log(person)
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+interface Person {
+    name: string;
+    additionInfo?: {
+      someInfo: string;
     }
-
-    abstract openDoor(key: Key): boolean
-}
-
-class MyHouse extends House {
-    openDoor(key: Key) {
-        if (key.getSignature() !== this.key.getSignature()) {
-            throw new Error ('Key to another door')
-        }
-        return this.door = true;
+  }
+  
+  const user: Person = {
+    name: 'Alex',
+    additionInfo: {
+        someInfo: 'adwda'
     }
-};
-
-class Key {
-    private signature: number;
-
-    constructor () {
-        this.signature = Math.floor(Math.random()*10);
-    }
-
-    getSignature(): number {
-        return this.signature
-    }
-}
-
-class Person {
-    constructor (private key: Key) {}
-
-    getKey(): Key {
-        return  this.key
-    }
-}
-const key = new Key();
-console.log(key)
-
-const house = new MyHouse(key);
-console.log(house)
-
-const person = new Person(key);
-console.log(person)
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
+  }
+  
+  console.log(user?.additionInfo?.someInfo);
 
 
